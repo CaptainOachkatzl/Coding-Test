303b97f4f9dc1ab06a05f2b7ca141fb7434b0770
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-05-22 20:30:35 +0200

    bugfix

--- src/dispute_status.rs
+++ src/dispute_status.rs
@@ -2,7 +2,6 @@ use anyhow::{bail, Result};
 
 pub struct DisputeStatus {
   disputed: bool,
-  resolved: bool,
   charged_back: bool,
 }
 
@@ -10,7 +9,6 @@ impl DisputeStatus {
   pub fn new() -> Self {
     Self {
       disputed: false,
-      resolved: false,
       charged_back: false,
     }
   }
@@ -27,7 +25,7 @@ impl DisputeStatus {
   pub fn resolve(&mut self) -> Result<()> {
     self.verify_disputed()?;
 
-    self.resolved = true;
+    self.disputed = false;
     Ok(())
   }
 
@@ -41,10 +39,6 @@ impl DisputeStatus {
   fn verify_disputed(&self) -> Result<()> {
     if !self.disputed {
       bail!("not disputed");
-    }
-
-    if self.resolved {
-      bail!("already resolved")
     }
 
     if self.charged_back {
