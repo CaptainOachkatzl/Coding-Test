193ffa577522c975302ca352b5dd92e9ceaa0536
CaptainOachkatzl <dave.hofer@hotmail.com>
2022-05-23 07:41:27 +0200

    improved test case

--- tests/large_input_tests.rs
+++ tests/large_input_tests.rs
@@ -1,15 +1,27 @@
-use coding_test::{AccountStorage, TransactionParser};
+use coding_test::{Account, AccountStorage, TransactionParser};
 
 #[test]
 fn large_valid_test() {
   let mut account_storage = AccountStorage::new();
-  TransactionParser::parse_transactions_into_accounts("transaction_files/valid_mixed.csv", &mut account_storage)
-    .unwrap();
+  TransactionParser::parse_transactions_into_accounts("transaction_files/valid_mixed.csv", &mut account_storage).unwrap();
+  account_storage
+    .get_accounts()
+    .iter()
+    .for_each(|(_, acc)| assert_positive_balance(acc));
 }
 
 #[test]
 fn large_invalid_test() {
   let mut account_storage = AccountStorage::new();
-  TransactionParser::parse_transactions_into_accounts("transaction_files/invalid_mixed.csv", &mut account_storage)
-    .unwrap();
+  TransactionParser::parse_transactions_into_accounts("transaction_files/invalid_mixed.csv", &mut account_storage).unwrap();
+  account_storage
+    .get_accounts()
+    .iter()
+    .for_each(|(_, acc)| assert_positive_balance(acc));
+}
+
+fn assert_positive_balance(account: &Account) {
+  assert!(account.get_funds().available.is_sign_positive());
+  assert!(account.get_funds().held.is_sign_positive());
+  assert!(account.get_funds().get_total().is_sign_positive());
 }
